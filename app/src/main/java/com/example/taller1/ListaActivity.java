f40151c3ba4611e2f9af6c42897f8a342633651b
JuanDavid31 <juandavid0306@hotmail.com>
2019-02-23 16:29:55 -0500

    Primer y segunda activity agregadas

---
+++ app/src/main/java/com/example/taller1/ListaActivity.java
@@ -0,0 +1,68 @@
+package com.example.taller1;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
+
+public class ListaActivity extends AppCompatActivity {
+
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private boolean doubleBackToExitPressedOnce = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista);
+
+        getActionBar();
+
+        String nombre = getIntent().getStringExtra("nombre");
+        if(nombre != null){
+            setTitle(nombre);
+        }
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        String[] pantallas = new String[3];
+        pantallas[0] = "Pantalla 1";
+        pantallas[1] = "Pantalla 2";
+        pantallas[2] = "Pantalla 3";
+
+        mAdapter = new MyAdapter(pantallas);
+        recyclerView.setAdapter(mAdapter);
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed();
+            return;
+        }
+
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, "Presione otra vez para salir", Toast.LENGTH_SHORT).show();
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                doubleBackToExitPressedOnce=false;
+            }
+        }, 2000);
+    }
+}
--- app/src/main/java/com/example/taller1/MainActivity.java
+++ app/src/main/java/com/example/taller1/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.taller1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText etNombre;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etNombre = findViewById(R.id.editText);
+    }
+
+    public void ingresar(View view) {
+        if(etNombre.getText().toString() == null || etNombre.getText().toString().equalsIgnoreCase("")){
+            Toast.makeText(this, "Ingrese un nombre", Toast.LENGTH_SHORT).show();
+        }else{
+            Intent i = new Intent(this, ListaActivity.class);
+            i.putExtra("nombre", etNombre.getText().toString());
+            startActivity(i);
+        }
     }
 }
